9e13d0354d874f5eb54814ac41d57da9e108abf1
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-10-21 00:38:41 +0600

    integrated css in with react

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import './country.css';
 
 const Country = ({ country }) => {
 
-    console.log(country.languages.languages.ara)
+    console.log(country.languages.languages.eng)
+
+    const handleVisited = () =>{
+        console.log("Button Clicked");
+    }
     return (
-        <div >
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+        <div className='country'>
+            <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
             <p>Name: {country.name.common}</p>
             <p>Capital: {country.capital.capital}</p>
             <p>Population: {country.population.population}</p>
-            <p>Languages: {country.region.region}</p> 
+            <p>Languages: {country.region.region}</p>
+            <p>Area: {country.area.area}   {country.area.area > 3000 ? " ⭐Big country" : "😒Small country"}</p>
+
+            <button onClick={handleVisited}>👩 Not visited</button>
 
         </div>
     );
 };
 
 export default Country;
+
+
+/**
+ * 1.inline css
+ * 2.
+ */
